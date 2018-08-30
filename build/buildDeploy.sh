1f71917be63b2d1deda9b55a0fbe4ab530c83a14
AutoDeploy <no@nowrite.com>
2018-08-31 01:10:44 +0400

    split AutoDeploy from svn rev.140

--- build/buildDeploy.sh
+++ build/buildDeploy.sh
@@ -6,7 +6,7 @@
         exit 100
     fi
 
-    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
     cd "${1}/build"
 
@@ -24,5 +24,16 @@
     rc=$?
 
     rm -f ./testDeployBuild
+
+    if [[ "${rc}" != "0" ]] ;
+    then
+        echo "${3} rev.${2} - test/part stage error: ${rc}"
+        exit ${rc}
+    fi
+
+    cd ..
+    /usr/bin/git add .
+    /usr/bin/git commit -m"split AutoDeploy from ${3} rev.${2}" --amend --author="AutoDeploy <no@nowrite.com>"
+    /usr/bin/git push origin
 
     exit ${rc}
